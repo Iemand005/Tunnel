d7b8b95136660f23eaa7865e8ccc353ba329926c
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-15 20:33:42 +0200

    bruh dude I had hardcode the paths again mybad

--- Tunnel.hpp
+++ Tunnel.hpp
@@ -75,7 +75,7 @@ public:
 
 		SetClearColor(1, 1, 0);
 
-		LoadShaders("/home/lasse/Documents/Projects/FenixEngine/src/games/Tunnel/resources/shaders/VertexShader.glsl", "/home/lasse/Documents/Projects/FenixEngine/src/games/Tunnel/resources/shaders/FragmentShader.glsl");
+		LoadShaders("resources/shaders/VertexShader.glsl", "resources/shaders/FragmentShader.glsl");
 
 		LoadModels();
 
