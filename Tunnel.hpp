eb7d3f66cc3e6960be7a0d4e830d8758495bc600
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-26 00:28:04 +0200

    GetWindow

--- Tunnel.hpp
+++ Tunnel.hpp
@@ -243,7 +243,7 @@ public:
 
 	void ProcessInput() {
 		SDL_Event event;
-		fe::SDLWindow *window = (fe::SDLWindow*)this->window.get();
+		fe::SDLWindow *window = GetWindow<fe::SDLWindow>();
 		while (window->PollSDLEvent(&event)) {
 			ImGui_ImplSDL3_ProcessEvent(&event);
 			auto io = ImGui::GetIO();
